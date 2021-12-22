6c964d7a0306cde899ef61c5fe3717556d642db5
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2021-12-22 11:34:27 +0100

    Add test subject role to joining members
    forgot that one

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,9 +1,10 @@
-const { joinMessages, config: { generalChannelID, rulesChannelID } } = require('../../constants.json');
+const { joinMessages, config: { generalChannelID, rulesChannelID, tofuSubjectRoleID } } = require('../../constants.json');
 const { setStatus } = require('../../utils.js');
 
 module.exports = async (client, member) => {
-	setStatus(client);
+	member.roles.add(tofuSubjectRoleID);
 	const randomWelc = joinMessages[Math.floor(Math.random() * joinMessages.length)];
 	const formatWelc = randomWelc.replace('{user}', `<@${member.id}>`);
 	client.channels.cache.get(generalChannelID).send(`${formatWelc}\nTry checking <#${rulesChannelID}>`);
+	setStatus(client);
 };
